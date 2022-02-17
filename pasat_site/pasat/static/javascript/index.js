b9d24185999cb2e11a79cc511be1f693a73a7aa4
steven300900 <stevenkristian300900@gmail.com>
2022-02-17 21:03:42 +0700

    Added functionality to show current number that user should input

--- pasat_site/pasat/static/javascript/index.js
+++ pasat_site/pasat/static/javascript/index.js
@@ -1,51 +1,102 @@
-var Game = /** @class */ (function () {
-    function Game() {
+"use strict";
+class Game {
+    constructor() {
         this.currentlyPlaying = false;
+        this.rounds = 30;
+        this.score = 0;
+        this.speed = 2000;
+        this.limit = 9;
     }
-    Game.prototype.toggle = function () {
+    changeGameSettings(rounds, speed, limit) {
+        this.rounds = rounds;
+        this.speed = speed;
+        this.limit = limit;
+    }
+    toggle() {
         if (this.currentlyPlaying == false) {
+            document.getElementById("button_play").textContent = "Pause";
             this.currentlyPlaying = true;
         }
         else {
+            document.getElementById("button_play").textContent = "Play";
             this.currentlyPlaying = false;
         }
-    };
-    return Game;
-}());
-var UserInput = /** @class */ (function () {
-    function UserInput() {
+    }
+    playAudio(audioHTMLObject) {
+        console.log(audioHTMLObject);
+        var audio = new Audio(audioHTMLObject);
+        audio.play();
+    }
+    pauser() {
+        console.log("Pausing");
+    }
+}
+class UserInput {
+    constructor() {
         this.currentNumber = 0;
     }
-    return UserInput;
-}());
-var Numpad = /** @class */ (function () {
-    function Numpad(numpadName, inputNumber) {
+}
+class Numpad {
+    constructor(numpadName, inputNumber) {
         this.numpadName = numpadName;
         this.inputNumber = inputNumber;
     }
-    Numpad.prototype.setNumpadFunctinoality = function (numpad) {
+    setNumpadFunctinoality(numpad) {
         var number = document.getElementById(this.numpadName);
         number.addEventListener("click", function () {
             console.log(numpad.inputNumber);
         });
-    };
-    return Numpad;
-}());
+    }
+}
 function main() {
+    // Start the game
     var game = new Game();
     var startButton = document.getElementById("button_play");
     startButton.addEventListener("click", function () {
         game.toggle();
+        var numberList = new Map();
+        numberList.set(0, 0);
+        numberList.set(1, 0);
+        var curRounds = 0;
+        var curNumber = 0;
+        var intervalID1 = setInterval(function () {
+            if (curRounds == game.rounds)
+                window.clearInterval(intervalID1);
+            if (curRounds % 2 == 0) {
+                curNumber = Math.floor(Math.random() * (game.limit - numberList.get(1) + 1)) + 0;
+                numberList.set(0, curNumber);
+                console.log(numberList.get(0));
+            }
+            else {
+                curNumber = Math.floor(Math.random() * (game.limit - numberList.get(0) + 1)) + 0;
+                numberList.set(1, curNumber);
+                console.log(numberList.get(1));
+            }
+            game.playAudio(numberAndFilenameMap.get(curNumber));
+            console.log(`Rounds: ${curRounds} Answer: ${numberList.get(0) + numberList.get(1)}`);
+            curRounds += 1;
+        }, game.speed);
+        // setTimeout(() => {
+        //     var intervalID2 = setInterval(function(){
+        //         if(curRounds == game.rounds / 2){
+        //             window.clearInterval(intervalID2);
+        //         }
+        //         numberList.set(1, Math.floor(Math.random() * (game.limit - 0 + 1)) + 0);
+        //         game.playAudio(numberAndFilenameMap.get(numberList.get(1)));
+        //     }, game.speed * 2);
+        // }, game.speed);
     });
+    // Set the numpad to its functionality
     var numpadArrayName = ["button_zero", "button_one", "button_two", "button_three", "button_four", "button_five", "button_six", "button_seven", "button_eight", "button_nine"];
-    var numpadArray = [];
     for (var i = 0; i < numpadArrayName.length; i++) {
         var numpad = new Numpad(numpadArrayName[i], i);
         numpad.setNumpadFunctinoality(numpad);
-        numpadArray.push(numpad);
-        console.log("Done");
     }
-    // let userInput = document.getElementById("user-input");
+    const numberAndFilenameMap = new Map();
+    var filenameSFX = ["sfx_0", "sfx_1", "sfx_2", "sfx_3", "sfx_4", "sfx_5", "sfx_6", "sfx_7", "sfx_8", "sfx_9"];
+    var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (var i = 0; i < filenameSFX.length; i++) {
+        numberAndFilenameMap.set(number[i], "/static/sfx/" + filenameSFX[i] + ".wav");
+    }
 }
-console.log("TEST");
 main();
--- pasat_site/pasat/static/javascript/index.ts
+++ pasat_site/pasat/static/javascript/index.ts
@@ -1,17 +1,43 @@
 class Game {
     currentlyPlaying: boolean;
+    rounds: number;
+    score: number;
+    speed: number;
+    limit: number;
 
     constructor(){
         this.currentlyPlaying = false;
+        this.rounds = 30;
+        this.score = 0;
+        this.speed = 2000;
+        this.limit = 9;
     }
 
-    toggle(): void{
+    changeGameSettings(rounds: number, speed: number, limit: number){
+        this.rounds = rounds;
+        this.speed = speed;
+        this.limit = limit;
+    }
+
+    toggle(){
         if(this.currentlyPlaying == false){
+            document.getElementById("button_play")!.textContent = "Pause";
             this.currentlyPlaying = true;
         }
         else{
+            document.getElementById("button_play")!.textContent = "Play";
             this.currentlyPlaying = false;
         }
+    }
+
+    playAudio(audioHTMLObject: string){
+        console.log(audioHTMLObject);
+        var audio = new Audio(audioHTMLObject);
+        audio.play();
+    }
+
+    pauser(){
+        console.log("Pausing");
     }
 }
 
@@ -33,33 +59,63 @@ class Numpad {
     }
 
     setNumpadFunctinoality(numpad: Numpad){
-        let number = document.getElementById(this.numpadName);
+        var number = document.getElementById(this.numpadName)!;
         number.addEventListener("click", function(){
             console.log(numpad.inputNumber);
         }); 
     }
-
-
 }
 
 function main(){
+    // Start the game
     var game = new Game();
-    var startButton = document.getElementById("button_play");
+    var startButton = document.getElementById("button_play")!;
+
     startButton.addEventListener("click", function () {
         game.toggle();
+        
+        var numberList = new Map();
+        numberList.set(0, 0);
+        numberList.set(1, 0);
+
+        var curRounds = 0;
+        var curNumber = 0;
+        var intervalID1 = setInterval(function(){
+            if(curRounds == game.rounds) window.clearInterval(intervalID1);
+            
+            if(curRounds % 2 == 0){
+                curNumber = Math.floor(Math.random() * (game.limit - numberList.get(1) + 1)) + 0;
+                numberList.set(0, curNumber);
+                console.log(numberList.get(0));
+            }
+            else{
+                curNumber = Math.floor(Math.random() * (game.limit - numberList.get(0) + 1)) + 0;
+                numberList.set(1, curNumber);
+                console.log(numberList.get(1));
+            }
+            
+            game.playAudio(numberAndFilenameMap.get(curNumber));
+
+            console.log(`Rounds: ${curRounds} Answer: ${numberList.get(0) + numberList.get(1)}`);
+            curRounds += 1;
+        }, game.speed);
     });
     
+    // Set the numpad to its functionality
     var numpadArrayName = ["button_zero", "button_one", "button_two", "button_three", "button_four", "button_five", "button_six", "button_seven", "button_eight", "button_nine"];
-
-    var numpadArray = [];
     for (var i = 0; i < numpadArrayName.length; i++) {
         var numpad = new Numpad(numpadArrayName[i], i);
         numpad.setNumpadFunctinoality(numpad);
-        numpadArray.push(numpad);
-        console.log("Done");
     }
-    // let userInput = document.getElementById("user-input");
+
+    const numberAndFilenameMap = new Map();
+    var filenameSFX = ["sfx_0", "sfx_1", "sfx_2", "sfx_3", "sfx_4", "sfx_5", "sfx_6", "sfx_7", "sfx_8", "sfx_9"];
+    var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for(var i = 0; i < filenameSFX.length; i++){
+        numberAndFilenameMap.set(number[i], "/static/sfx/" + filenameSFX[i] + ".wav");
+    }
+
+
 }
 
-console.log("TEST");
 main();
