60762953b7f7f1fa9ef80eeded19572f12958e2e
steven300900 <stevenkristian300900@gmail.com>
2022-02-19 08:28:46 +0700

    Added pasat as path

--- pasat_site/pasat/urls.py
+++ pasat_site/pasat/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pasat', views.index, name='index'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
